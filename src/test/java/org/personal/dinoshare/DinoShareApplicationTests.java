314dd1e54bbfe442758127b03b09983ee3286161
goeunpark123 <64297541+goeunpark123@users.noreply.github.com>
2025-05-03 22:15:37 +0900

    initial commit

---
+++ src/test/java/org/personal/dinoshare/DinoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.dinoshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoShareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
